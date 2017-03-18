97ccf2d4b7290439cc74f9e565ac0323f02e0bcd
Thalon <thalon@gmx.at>
2017-03-18 15:55:01 +0100

    Übung2

---
+++ lib/Bookshop/SessionContext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thalon
+ * Date: 18.03.2017
+ * Time: 12:12
+ */
+
+namespace Bookshop;
+
+
+class SessionContext
+{
+
+}
---
+++ lib/Bookshop/ShoppingCart.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thalon
+ * Date: 18.03.2017
+ * Time: 12:08
+ */
